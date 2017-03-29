7e8f8dda82590ca881f4dfbb5ec87a1146ee54d4
NaomiMadeIt <unknownobjectremix@gmail.com>
2017-03-29 16:08:45 -0700

    First
    Really basic, starting up my first WordPress website/theme.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>WEBSITE TITLE</title>
+  </head>
+  <body>
+    <header>
+      <div class="mainlogo"></div>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></
+            li>
+          <li><a href="#">About Me</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contact Me</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+  </body>
+</html>
